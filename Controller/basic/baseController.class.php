9885a6dc6cddb9797539ec75b472ac99438f696c
dama1985 <66268540@163.com>
2016-07-16 16:58:24 +0800

    基础类控制器

---
+++ Controller/basic/baseController.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/7/15 0015
+ * Time: 15:59
+ */
+//controller/basic里面的类可以按命名，按照配置文件中的元素顺序继承，也可以继承
+namespace Controller\basic;
+use Core\base as base;
+class baseController extends base
+{
+    public function __initialize()
+    {
+        
+    }
+}
